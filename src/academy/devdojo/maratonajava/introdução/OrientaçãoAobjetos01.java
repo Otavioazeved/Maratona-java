6dd6a9cd82d2e133f84ed679391fae5c17aef3c6
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-01-26 20:56:41 -0300

    inIniciando em poo

--- src/academy/devdojo/maratonajava/introdução/OrientaçãoAobjetos01.java
+++ src/academy/devdojo/maratonajava/introdução/OrientaçãoAobjetos01.java
@@ -2,6 +2,6 @@ package academy.devdojo.maratonajava.introdução;
 
 public class OrientaçãoAobjetos01 {
     public static void main(String[] args) {
-
+        System.out.println("ola mundo");
     }
 }
