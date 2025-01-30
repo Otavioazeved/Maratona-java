599e8cf030fcb79f265ad7b8fce449f32e22dbc2
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-01-29 22:58:54 -0300

    Iniciando em métodos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/Test/CalculdoraTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Bintroduçãométodos.Test;
+
+import academy.devdojo.maratonajava.javacore.Bintroduçãométodos.dominio.Calculadora;
+
+public class CalculdoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora =  new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+        calculadora.multiplicandoDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+
+
+
+    }
+
+
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/dominio/Calculadora.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Bintroduçãométodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros(){
+        System.out.println(10 + 10);
+    }
+    public void subtraiDoisNumeros(){
+        System.out.println(10 - 8);
+    }
+    public void multiplicandoDoisNumeros(){
+        System.out.println(10 * 9);
+    }
+
+}
