e006442e8f2ac5058c7715ae816ca8d22fa66c8f
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-02-03 18:40:21 -0300

    testando e testando parametros tipo primitivo

--- src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/Test/CalculadoraTest04.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/Test/CalculadoraTest04.java
@@ -1,15 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Bintroduçãométodos.Test;
 
+
 import academy.devdojo.maratonajava.javacore.Bintroduçãométodos.dominio.Calculadora;
 
 public class CalculadoraTest04 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        int num1 = 1;
-        int num2 = 2;
-        calculadora.alteraDoisNumeros(num1, num2);
-        System.out.println("calculadoraTest04");
-        System.out.println("num1 " + num1);
-        System.out.println("num2" + num2);
+        int ori1 = 456;
+        int ori2 = 789;
+        calculadora.alterandNumerosTest(ori1, ori2);
+        System.out.println("depois do metodo");
+        System.out.println(ori1);
+        System.out.println(ori2);
     }
+
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/dominio/Calculadora.java
@@ -33,21 +33,24 @@ public class Calculadora {
 
     public double divideDoisNumeros2(double num1, double num2) {
         if (num1 == 0 || num2 == 0) {
-           return 0;
+            return 0;
         } else {
             return num1 / num2;
         }
 
     }
-    public void alteraDoisNumeros (int num1, int num2){
-        num1 = 99;
-        num2 = 33;
-        System.out.println("dentro do altera dois numeros: ");
+
+
+    public void alterandNumerosTest(int num1, int num2) {
+        num1 = 123;
+        num2 = 321;
+        System.out.println("dentro do metodo");
         System.out.println("num1 " + num1);
         System.out.println("num2 " + num2);
-
     }
+
 }
+
 
 
 
