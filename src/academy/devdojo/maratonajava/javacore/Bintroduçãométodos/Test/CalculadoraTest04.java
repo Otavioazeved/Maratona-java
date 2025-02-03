ab18b2da6bb951f664746dec157d9c65936aaadb
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-02-03 18:06:28 -0300

    parametros tipo primitivo

--- src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/Test/CalculadoraTest04.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/Test/CalculadoraTest04.java
@@ -10,7 +10,7 @@ public class CalculadoraTest04 {
         calculadora.alteraDoisNumeros(num1, num2);
         System.out.println("calculadoraTest04");
         System.out.println("num1 " + num1);
-        System.out.println("num2 "  + num2);
+
 
     }
 }
