a2350ed2ae8d4da5d5e5f77a1b2eacd936684034
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-03-21 17:10:31 -0300

    mais sobre parame. referencia

--- src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/dominio/ImpressoraEstudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãométodos/dominio/ImpressoraEstudante.java
@@ -4,7 +4,7 @@ public class ImpressoraEstudante {
     public void imprime(Estudante estudante){
 
         System.out.println("----------------");
-
+        estudante.name = "gohan";
         System.out.println(estudante.name);
         System.out.println(estudante.age);
         System.out.println(estudante.sex);
